28c4f471fafba9048b1ba61388d2227fce39690e
eraser5th <83107074+eraser5th@users.noreply.github.com>
2023-03-17 16:42:17 +0900

    cssをvanilla-extractに移行したりなどした

---
+++ src/global.css.ts
@@ -0,0 +1,60 @@
+import { globalStyle } from "@vanilla-extract/css"
+
+globalStyle("#root", {
+  maxWidth: "1280px",
+  margin: "0 auto",
+  padding: "2rem",
+  textAlign: "center",
+})
+
+globalStyle(":root", {
+  fontFamily: "Inter, system-ui, Avenir, Helvetica, Arial, sans-serif",
+  lineHeight: 1.5,
+  fontWeight: 400,
+  colorScheme: "light dark",
+  color: "rgba(255, 255, 255, 0.87)",
+  backgroundColor: "#242424",
+  fontSynthesis: "none",
+  textRendering: "optimizeLegibility",
+  WebkitFontSmoothing: "antialiased",
+  MozOsxFontSmoothing: "grayscale",
+  WebkitTextSizeAdjust: "100%",
+})
+
+globalStyle("a", {
+  fontWeight: 500,
+  color: "#646cff",
+  textDecoration: "inherit",
+})
+
+globalStyle("a:hover", {
+  color: "#535bf2",
+})
+
+globalStyle("body", {
+  margin: 0,
+  display: "flex",
+  placeItems: "center",
+  minWidth: "320px",
+  minHeight: "100vh",
+})
+
+globalStyle("button", {
+  borderRadius: "8px",
+  border: "1px solid transparent",
+  padding: "0.6em 1.2em",
+  fontSize: "1em",
+  fontWeight: 500,
+  fontFamily: "inherit",
+  backgroundColor: "#1a1a1a",
+  cursor: "pointer",
+  transition: "border-color 0.25s",
+})
+
+globalStyle("button:hover", {
+  borderColor: "#646cff",
+})
+
+globalStyle("button:focus-visible", {
+  outline: "4px auto -webkit-focus-ring-color",
+})
