9f5d0d3147bcc186627461c4de6889c5b456c859
eraser5th <83107074+eraser5th@users.noreply.github.com>
2023-03-17 18:07:21 +0900

    プロフィールを仮決め

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,123 @@ export const App = () => {
   return (
     <div className="App">
       <img src={icon} alt="me" className="icon" />
+      <h1>eraser5th</h1>
+      <h2>プロフィール</h2>
+      <p>
+        本名は加藤豪（たけし）。会津大学学部２年、Zliサークル所属。
+        <br />
+        Webフロントエンドの開発を主にやっています。
+        <br />
+        趣味はゲームとアニメとVtuberと環境構築(Neovim, Wezterm, SketchyBar,
+        etc)。
+      </p>
+      <h2>スキル</h2>
+      <h3>言語など</h3>
+      <ul>
+        <li>JavaScript</li>
+        <li>TypeScript</li>
+        <li>CSS</li>
+        <li>HTML</li>
+        <li>Lua</li>
+        <li>C・C++</li>
+        <li>Kotlin</li>
+        <li>Java</li>
+        <li>GraphQL</li>
+        <li>REST API</li>
+        <li>WebSocket</li>
+        <li>Coq（定理証明）</li>
+        <li>Prisma（ORM）</li>
+      </ul>
+      <h3>フレームワーク・ライブラリなど</h3>
+      <ul>
+        <li>React・Next</li>
+        <li>Vue・Nuxt</li>
+        <li>Bootstrap</li>
+        <li>tailwind</li>
+        <li>Apollo Client</li>
+        <li>Vite</li>
+      </ul>
+      <h3>ツールなど</h3>
+      <ul>
+        <li>Git・GitHub</li>
+        <li>AWS Lambda</li>
+        <li>Google App Engine</li>
+      </ul>
+      <h2>経歴</h2>
+      <h3>インターン・アルバイト経験</h3>
+      <ul>
+        <li>2022/1 ~ 現在：Appify Technologiesにてアルバイト・インターン</li>
+      </ul>
+      <h3>ハッカソン</h3>
+      <ul>
+        <li>第２回100Program：参加</li>
+        <li>
+          わたしたちのウェルビーイングのためのハッカソン2021：富士通
+          つながる豊かさ賞受賞
+        </li>
+        <li>わたしたちのウェルビーイングのためのハッカソン2022：参加</li>
+        <li>Open Hack U 2021 Online Vol.3：参加</li>
+      </ul>
+      <h2>趣味・好きなもの</h2>
+      <h3>ゲーム</h3>
+      <ul>
+        <li>Splatoon</li>
+        <li>Monster Hunter</li>
+      </ul>
+      <h3>小説・アニメ・漫画など</h3>
+      <ul>
+        <li>Re:ゼロから始める異世界生活</li>
+        <li>ダンジョンに出会いを求めるのは間違っているだろうか</li>
+        <li>週末なにしてますか？忙しいですか？救ってもらっていいですか？</li>
+        <li>ギルティクラウン</li>
+        <li>TypeMoon作品</li>
+        <ul>
+          <li>空の境界</li>
+          <li>Fateシリーズ</li>
+          <li>月姫</li>
+        </ul>
+        <li>高橋留美子作品</li>
+        <ul>
+          <li>うる星やつら</li>
+          <li>めぞん一刻</li>
+          <li>人魚の森</li>
+        </ul>
+        <li>ジブリ作品</li>
+        <ul>
+          <li>風の谷のナウシカ</li>
+          <li>もののけ姫</li>
+        </ul>
+      </ul>
+      <h3>音楽</h3>
+      <ul>
+        <li>EGOIST</li>
+        <li>削除</li>
+        <li>SEKAI NO OWARI</li>
+      </ul>
+      <h3>環境構築・UNIX Porn</h3>
+      <ul>
+        <li>Neovim</li>
+        <li>SketchyBar</li>
+        <li>Wezterm</li>
+      </ul>
+      <h3>Vtuber</h3>
+      <ul>
+        <li>猫又おかゆ</li>
+        <li>常闇トワ</li>
+        <li>獅白ぼたん</li>
+        <li>Hakos Bealz</li>
+        <li>Calliope Mori</li>
+        <li>Ceres Fauna</li>
+        <li>大空スバル</li>
+        <li>大神ミオ</li>
+        <li>雪花ラミィ</li>
+        <li>ロボ子さん</li>
+        <li>兎田ぺこら</li>
+        <li>姫森ルーナ</li>
+        <li>風真いろは</li>
+        <li>天野ピカミィ</li>
+        <li>社築</li>
+      </ul>
     </div>
   )
 }
