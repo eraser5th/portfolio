d46a35d2572e2452c307430d963c23552d1d7e77
eraser5th <83107074+eraser5th@users.noreply.github.com>
2023-05-07 15:04:32 +0900

    add Page component

---
+++ src/components/Page.tsx
@@ -0,0 +1,9 @@
+import { page } from "./Page.css"
+
+export const Page: React.FC<{
+  children: React.ReactNode | React.ReactNode[]
+}> = ({ children })=> {
+  return <main className={page}>
+    {children}
+  </main>
+}
