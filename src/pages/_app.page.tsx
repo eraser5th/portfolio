11ab19842c51c5f7c7451ba6c5b43002603150c6
eraser5th <83107074+eraser5th@users.noreply.github.com>
2023-05-07 20:09:10 +0900

    some change

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from 'next/app';
 import "./_app.page.css"
-// import "../styles/globals.css"
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps}/>
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image"
 import { app, icon } from "./index.css"
-import "../styles/global.css"
 
 const AboutPage = () => {
   return (
