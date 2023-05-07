585082eee003ed7c65876e6c8288f9635f7772a2
eraser5th <83107074+eraser5th@users.noreply.github.com>
2023-05-07 15:05:13 +0900

    add About feature

---
+++ src/features/About/AboutPage.css.ts
@@ -0,0 +1,10 @@
+import { style } from "@vanilla-extract/css"
+
+export const app = style({
+  overflowWrap: "break-word",
+})
+
+export const icon = style({
+  width: "120px",
+  height: "120px",
+})
