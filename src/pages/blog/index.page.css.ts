1a596475e6ffcd2475eeffa9119a58dc9ed33e19
eraser5th <83107074+eraser5th@users.noreply.github.com>
2023-05-08 13:45:52 +0900

    update blog page

---
+++ src/pages/blog/index.page.css.ts
@@ -0,0 +1,6 @@
+import { style } from "@vanilla-extract/css";
+
+export const articleCard = style({
+  width: "100px",
+  height: "100px",
+})
--- src/pages/blog/index.page.tsx
+++ src/pages/blog/index.page.tsx
@@ -1,5 +1,40 @@
-const BlogPage: React.FC = () => {
-  return <div></div>
+import Link from "next/link"
+import { Page } from "../../components/Page"
+import { MyGetServerSideProps } from "../../types/MyGetServerSideProps"
+import { ArticleMeta, getAllArticleMeta } from "./articles/lib/posts"
+import { articleCard } from "./index.page.css"
+
+type BlogPageProps = {
+  articles: ArticleMeta[]
+}
+
+export const getServerSideProps: MyGetServerSideProps<BlogPageProps> = () => {
+  return {
+    props: { articles: getAllArticleMeta() },
+  }
+}
+
+const ArticleCard: React.FC<{
+  article: ArticleMeta
+}> = ({ article }) => {
+  const href = `/blog/articles/${article.id}`
+  return (
+    <Link key={article.id} href={href}>
+      <div className={articleCard}>
+        {article.title}
+      </div>
+    </Link>
+  )
+}
+
+const BlogPage: React.FC<BlogPageProps> = ({ articles }) => {
+  return (
+    <Page>
+      {articles.map((article) => (
+        <ArticleCard key={article.id} article={article} />
+      ))}
+    </Page>
+  )
 }
 
 export default BlogPage
