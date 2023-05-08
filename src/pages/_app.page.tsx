9d477012d6045f86dfa2edb3d69b58db18a501b9
eraser5th <83107074+eraser5th@users.noreply.github.com>
2023-05-08 12:49:59 +0900

    use RecoilContext

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,8 +1,13 @@
 import type { AppProps } from "next/app"
+import { RecoilRoot } from "recoil"
 import "./_app.page.css"
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  )
 }
 
 export default App
