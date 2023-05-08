f8aab1bf0bb94575e5e57c3007ebef686ed4bc72
eraser5th <83107074+eraser5th@users.noreply.github.com>
2023-05-08 12:50:23 +0900

    delete ogpContext

--- src/pages/Blog/articles/context/ogpContext.tsx
+++
@@ -1,7 +0,0 @@
-import { createContext } from "vm"
-import { OGP } from "../lib/getOgp"
-
-const ogpContext = createContext<OGP[]>([])
-export const OGPContextProvider = () => {
-  return <ogpContext.
-}
